9c4640a2f257baf91189ab971c3096bcba610b28
wyhaeck <wyhaeck@umich.edu>
2022-02-25 16:54:45 -0500

    Start on basics

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+import { CodeBlock } from 'react-code-blocks'
+import { Box } from '@material-ui/core'
 
 function App() {
   return (
@@ -12,6 +14,14 @@ function App() {
           <p>
             Hey {user.username}, welcome to my channel, with auth!
           </p>
+          <Box>
+            <CodeBlock
+              text="Hello"
+              language='typescript'
+              showLineNumbers
+              wrapLines
+            />
+          </Box>
           <button onClick={signOut}>Sign out</button>
         </div>
       )}
