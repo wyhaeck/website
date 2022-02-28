de369a866145b6dc95870a8810a7b63721bc5d90
wyhaeck <wyhaeck@umich.edu>
2022-02-28 14:25:05 -0500

    Added about and portfolio page text

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const AboutPage: React.FC = () => {
+  return (
+    <>
+      <p>
+        Hello! This is the about page.
+      </p>
+    </>
+  )
+}
+
+export default AboutPage;
---
+++ pages/portfolio.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const PortfolioPage: React.FC = () => {
+  return (
+    <>
+      <p>
+        Hello! This is the portfolio page.
+      </p>
+    </>
+  )
+}
+
+export default PortfolioPage;
